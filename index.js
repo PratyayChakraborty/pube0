d114531014113d751289a4ca729d8ebfa2c83e6f
poojakittu <poojayadav.yadav173@gmail.com>
2023-07-19 12:28:13 +0530

    added

--- index.js
+++ index.js
@@ -23,18 +23,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors({ origin: 'http://localhost:5000' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
