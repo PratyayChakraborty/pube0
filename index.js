ff85021edc9d34a6b8faf310eca944366bf156d7
poojakittu <poojayadav.yadav173@gmail.com>
2023-09-02 17:24:28 +0530

    added

--- index.js
+++ index.js
@@ -13,21 +13,28 @@ const ImageRoutes = require("./Routes/Image.Routes");
 const { EmailRoutes } = require("./Routes/Email.Routes");
 const { ContactRoutes } = require("./Routes/Contact.Routes");
 const { BlogRoutes } = require("./Routes/Blog.Routes");
-const {  TotalRoutes } = require("./Routes/Total.Routes");
+const { TotalRoutes } = require("./Routes/Total.Routes");
 const { AdminRoutes } = require("./Routes/Admin.Routes");
 const { SubscriptionRoutes } = require("./Routes/Subscription.Routes");
 const { OldPhonerouter } = require("./Routes/OldPhone.Routes");
-const promoRoutes = require('./Routes/PromoCode.Routes');
+const promoRoutes = require("./Routes/PromoCode.Routes");
 const { PolicyRouter } = require("./Routes/Policy.Routes");
 const { ReturnPolicyRouter } = require("./Routes/Returnpolicy.Routes");
 const { AgreementPolicyRouter } = require("./Routes/Agreement.Routes");
-const { beforePolicyRouter } = require("./Routes/BeforeStartPolicyModel.Routes");
+const {
+  beforePolicyRouter,
+} = require("./Routes/BeforeStartPolicyModel.Routes");
+const { userinfoRoutes } = require("./Routes/Userinfo.Routes");
 
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors("http://localhost:3001"));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,13 +42,12 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-
-app.use("/subscription",SubscriptionRoutes)
-app.use('/promos', promoRoutes);
-app.use("/admin",AdminRoutes)
-app.use("/old",OldPhonerouter)
-app.use("/total",TotalRoutes)
-app.use("/contact",ContactRoutes)
+app.use("/subscription", SubscriptionRoutes);
+app.use("/promos", promoRoutes);
+app.use("/admin", AdminRoutes);
+app.use("/old", OldPhonerouter);
+app.use("/total", TotalRoutes);
+app.use("/contact", ContactRoutes);
 app.use("/otp", require("./Routes/otp.routes"));
 app.use("/email", EmailRoutes);
 app.use("/img", ImageRoutes);
@@ -51,12 +57,13 @@ app.use("/comment", CommentRoutes);
 app.use("/cart", CartRoutes);
 app.use("/address", AddressRoutes);
 app.use("/order", OrderRoutes);
-app.use("/blog",BlogRoutes)
-app.use("/email",EmailRoutes)
-app.use("/policy",PolicyRouter)
-app.use("/returnpolicy",ReturnPolicyRouter)
-app.use("/agreemet",AgreementPolicyRouter)
-app.use("/beforePolicy",beforePolicyRouter)
+app.use("/blog", BlogRoutes);
+app.use("/email", EmailRoutes);
+app.use("/policy", PolicyRouter);
+app.use("/returnpolicy", ReturnPolicyRouter);
+app.use("/agreemet", AgreementPolicyRouter);
+app.use("/beforePolicy", beforePolicyRouter);
+app.use("/userinfo", userinfoRoutes);
 app.listen(process.env.port, async () => {
   try {
     await connection;
